722f0adce570b40f5e3ac78064fa1224ec40353c
jose <joseluis19963@gmail.com>
2020-10-06 00:11:33 -0500

    Update Helper JwtAuth

--- app/Helpers/JwtAuth.php
+++ app/Helpers/JwtAuth.php
@@ -29,12 +29,14 @@ class JwtAuth{
         //Generar el token con los datos  del usuario identificado
         if ($signup) {
             $token = array(
-                'sub' => $user->id,
-                'email' => $user->email,
-                'name' => $user->name,
-                'surname' => $user->surname,
-                'iat' => time(),
-                'exp' => time() + (7 * 24 * 60 * 60)
+                'sub'         => $user->id,
+                'email'       => $user->email,
+                'name'        => $user->name,
+                'surname'     => $user->surname,
+                'description' => $user->description,
+                'image'       => $user->image,
+                'iat'         => time(),
+                'exp'         => time() + (7 * 24 * 60 * 60)
             );
 
             $jwt = JWT::encode($token, $this->key, 'HS256');
